6d60e185fde00d2869a1468fae83c5beed3848df
JonMourolin <j.prod.mourolin@gmail.com>
2025-04-26 22:23:32 +0200

    fix: Remove duplicate infinite tab file causing import error

--- app/(tabs)/infinite.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// Importer le composant partagé (le chemin est correct depuis app/(tabs)/)
-import { InfiniteScreen } from '../infinite';
-
-// L'écran pour l'onglet mobile rend simplement le composant partagé
-export default function InfiniteTabScreen() {
-  return <InfiniteScreen />;
-} 
